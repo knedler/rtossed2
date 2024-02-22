c10c4796f1e764d760bffb84a6675a5a0df7a02f
Blake <blake.knedler@maine.edu>
2024-02-22 11:52:49 -0500

    Replace HAL_Delay

--- CM7/Core/Src/user_syscalls.c
+++ CM7/Core/Src/user_syscalls.c
@@ -3,13 +3,10 @@
 #include "user_syscalls.h"
 #include "process.h"
 
-int microsleep(uint32_t msec)
-{
-	// Convert from ms to us
-	uint32_t usec = msec * 1000;
-	
-	// Get current and wakeup time 
-	current->w_time = uwTick + usec;
+int microsleep(uint32_t usec)
+{	
+	// Get wakeup time after converting to micro sec 
+	current->w_time = (uwTick * 1000) + usec;
 
 	// Change the state
 	current->state &= ~STATE_TIME_SLEEP;
--- Common/Src/pdblink.c
+++ Common/Src/pdblink.c
@@ -2,12 +2,13 @@
 
 #include "pdblink.h"
 #include "gpio.h"
+#include "user_syscalls.h"
 
 int pdblink(void)
 {
 	while (1) {
 		HAL_GPIO_TogglePin(D3_GPIO_Port, D3_Pin);
 
-		HAL_Delay(50);
+		microsleep(50000);
 	}
 }
